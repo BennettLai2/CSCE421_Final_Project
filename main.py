26c1d12b1fc91b29b83c0c057dca109f675e1fb0
BWSullivan <brandon71399@gmail.com>
2023-04-29 17:18:31 -0500

    CUDA Acceleration

--- main.py
+++ main.py
@@ -14,8 +14,33 @@ from parser_1 import parse
 from model import Model
 
 
+def get_default_device():
+    if torch.cuda.is_available():
+        return torch.device('cuda')
+    else:
+        return torch.device('cpu')
+
+def to_device(data, device):
+    if isinstance(data, (list, tuple)):
+        return [to_device(x, device) for x in data]
+    return data.to(device, non_blocking=True)
+
+class DeviceDataLoader():
+    def __init__(self, dl, device):
+        self.dl = dl
+        self.device = device
+
+    def __iter__(self):
+        for b in self.dl:
+            yield to_device(b, self.device)
+
+    def __len__(self):
+        return len(self.dl)
+
 def main():
     args = parse()
+    device = get_default_device()
+    print(device)
 
     x = load_data("train_x.csv")
     y = load_data("train_y.csv")
