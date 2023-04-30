7f967d19a05ef984556663afeda08fbaca5262d9
BWSullivan <brandon71399@gmail.com>
2023-04-29 19:32:36 -0500

    Data V1

--- data.py
+++ data.py
@@ -88,13 +88,40 @@ def preprocess_x(df):
     # condensed_df['Capillary Refill'] = condensed_df['Capillary Refill'].fillna(0, inplace=True)
     
     condensed_df['Capillary Refill'] = condensed_df.groupby('patientunitstayid')['Capillary Refill'].ffill().fillna(0)
+    condensed_df['Heart Rate'] = condensed_df.groupby('patientunitstayid')['Heart Rate'].ffill()
+    condensed_df['Heart Rate'] = condensed_df.groupby('patientunitstayid')['Heart Rate'].ffill()
+    condensed_df['O2 Saturation'] = condensed_df.groupby('patientunitstayid')['O2 Saturation'].ffill()
+    condensed_df['O2 Saturation'] = condensed_df.groupby('patientunitstayid')['O2 Saturation'].bfill()
+    condensed_df['O2 Saturation'] = condensed_df['O2 Saturation'].fillna(100)
+    condensed_df['Respiratory Rate'] = condensed_df.groupby('patientunitstayid')['Respiratory Rate'].ffill()
+    condensed_df['Respiratory Rate'] = condensed_df.groupby('patientunitstayid')['Respiratory Rate'].bfill()
+    condensed_df['Respiratory Rate'] = condensed_df['Respiratory Rate'].fillna(18)
     condensed_df['glucose'] = condensed_df.groupby('patientunitstayid')['glucose'].ffill()
-    condensed_df['glucose'] = condensed_df.groupby('patientunitstayid')['glucose'].ffill()
+    condensed_df['glucose'] = condensed_df.groupby('patientunitstayid')['glucose'].bfill()
     condensed_df['glucose'] = condensed_df['glucose'].fillna(86.6)
     condensed_df['pH'] = condensed_df.groupby('patientunitstayid')['pH'].ffill()
     condensed_df['pH'] = condensed_df.groupby('patientunitstayid')['pH'].bfill()
     condensed_df['pH'] = condensed_df['pH'].fillna(7.4)
     
     condensed_df['GCS Total'] = condensed_df.groupby('patientunitstayid')['GCS Total'].ffill().fillna(15)
+
+    combined_df = condensed_df[['Invasive BP Diastolic', 'Invasive BP Mean', 'Invasive BP Systolic', 'Non-Invasive BP Diastolic', 'Non-Invasive BP Mean', 'Non-Invasive BP Systolic']].copy()
+    combined_df['BP Diastolic'] = combined_df['Invasive BP Diastolic'].combine_first(combined_df['Non-Invasive BP Diastolic'])
+    combined_df['BP Mean'] = combined_df['Invasive BP Mean'].combine_first(combined_df['Non-Invasive BP Mean'])
+    combined_df['BP Systolic'] = combined_df['Invasive BP Systolic'].combine_first(combined_df['Non-Invasive BP Systolic'])
+    combined_df = combined_df[['BP Diastolic', 'BP Mean', 'BP Systolic']]
+
+    condensed_df.drop(['Invasive BP Diastolic', 'Invasive BP Mean', 'Invasive BP Systolic', 'Non-Invasive BP Diastolic',
+                       'Non-Invasive BP Mean', 'Non-Invasive BP Systolic'], axis=1, inplace=True)
+    condensed_df = pd.concat([condensed_df, combined_df], axis=1)
+    condensed_df['BP Diastolic'] = condensed_df.groupby('patientunitstayid')['BP Diastolic'].ffill()
+    condensed_df['BP Diastolic'] = condensed_df.groupby('patientunitstayid')['BP Diastolic'].bfill()
+    condensed_df['BP Systolic'] = condensed_df.groupby('patientunitstayid')['BP Systolic'].ffill()
+    condensed_df['BP Systolic'] = condensed_df.groupby('patientunitstayid')['BP Systolic'].bfill()
+    condensed_df['BP Mean'] = condensed_df['BP Mean'].fillna(condensed_df['BP Diastolic'].shift(1) + (condensed_df['BP Diastolic'].shift(1)-condensed_df['BP Systolic'].shift(-1))/3)
+
+
+
     condensed_df.to_csv('processed_train_x.csv', index=False)
+
     return condensed_df
