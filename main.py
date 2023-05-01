6b11539067572c0e30ddc68251f169327b980f27
BennettLai2 <bxl190020@tamu.edu>
2023-05-01 16:32:17 -0500

    init

--- main.py
+++ main.py
@@ -68,30 +68,30 @@ def main():
     # ############################
 
     
-    skf=StratifiedKFold(n_splits=5, shuffle=False)
-    for train_index, test_index in skf.split(x, y): 
-        X_train, X_test = x.iloc[train_index], x.iloc[test_index]
-        Y_train = y[train_index]
-        Y_test = y[test_index]
+    # skf=StratifiedKFold(n_splits=5, shuffle=False)
+    # for train_index, test_index in skf.split(x, y): 
+    #     X_train, X_test = x.iloc[train_index], x.iloc[test_index]
+    #     Y_train = y[train_index]
+    #     Y_test = y[test_index]
         
-        cv_model = Model(50)  # you can add arguments as needed
-        acc = cv_model.fit(X_train.drop('patientunitstayid', axis=1), Y_train, X_test.drop('patientunitstayid', axis=1), Y_test)
-        print(acc)
+    #     cv_model = Model(50)  # you can add arguments as needed
+    #     acc = cv_model.fit(X_train.drop('patientunitstayid', axis=1), Y_train, X_test.drop('patientunitstayid', axis=1), Y_test)
+    #     print(acc)
     model = Model(50)  # you can add arguments as needed
     acc = model.fit(train_x.drop('patientunitstayid', axis=1), train_y, test_x.drop('patientunitstayid', axis=1), test_y)
         
 
-    # probas = pd.DataFrame(model.predict_proba(test_x.drop('patientunitstayid', axis=1)), columns=['proba_0', 'proba_1'])
-    # probas = probas[['proba_0']].values.ravel()
-    # np.savetxt('probas.csv', probas, delimiter=',')
-    # # print(test_x)
-    # patientunitstayid = test_x[['patientunitstayid']].values.ravel()
-    # unique_ids = np.unique(patientunitstayid)
-    # mean_proba = [np.mean(probas[np.where(patientunitstayid==id)]) for id in unique_ids]
+    probas = pd.DataFrame(model.predict_proba(test_x.drop('patientunitstayid', axis=1)), columns=['proba_0', 'proba_1'])
+    probas = probas[['proba_1']].values.ravel()
+    np.savetxt('probas.csv', probas, delimiter=',')
+    # print(test_x)
+    patientunitstayid = test_x[['patientunitstayid']].values.ravel()
+    unique_ids = np.unique(patientunitstayid)
+    mean_proba = [np.max(probas[np.where(patientunitstayid==id)]) for id in unique_ids]
 
-    # # # create 2D array with patientunitstayid and mean proba values
-    # result = np.column_stack((unique_ids, mean_proba))
-    # np.savetxt('result.csv', result, delimiter=',')
+    # # create 2D array with patientunitstayid and mean proba values
+    result = np.column_stack((unique_ids, mean_proba))
+    np.savetxt('result.csv', result, delimiter=',')
     
     # ---------------------------------------------
     # test_x = load_data("test_x.csv")
@@ -102,18 +102,18 @@ def main():
     # test_x = test_x.reindex(columns =['patientunitstayid', 'ethnicity_African American', 'ethnicity_Asian', 'ethnicity_Caucasian', 'ethnicity_Hispanic', 'ethnicity_Native American', 'ethnicity_Other/Unknown', 'gender_Female', 'gender_Male', 'unitvisitnumber', 'offset', 'admissionheight', 'admissionweight', 'age', 'Capillary Refill', 'GCS Total', 'Heart Rate', 'O2 Saturation', 'Respiratory Rate', 'glucose', 'pH', 'BP Diastolic', 'BP Mean', 'BP Systolic'])
     # test_x.to_csv('processed_test_x.csv', index=False)
 
-    test_x = load_data("processed_test_x.csv")
-    probas = pd.DataFrame(model.predict_proba(test_x.drop(['patientunitstayid'], axis=1)), columns=['proba_0', 'proba_1'])
-    probas = probas[['proba_1']].values.ravel()
-    np.savetxt('probas.csv', probas, delimiter=',')
-    # print(test_x)
-    patientunitstayid = test_x[['patientunitstayid']].values.ravel()
-    unique_ids = np.unique(patientunitstayid)
-    mean_proba = [np.mean(probas[np.where(patientunitstayid==id)]) for id in unique_ids]
+    # test_x = load_data("processed_test_x.csv")
+    # probas = pd.DataFrame(model.predict_proba(test_x.drop(['patientunitstayid'], axis=1)), columns=['proba_0', 'proba_1'])
+    # probas = probas[['proba_1']].values.ravel()
+    # np.savetxt('probas.csv', probas, delimiter=',')
+    # # print(test_x)
+    # patientunitstayid = test_x[['patientunitstayid']].values.ravel()
+    # unique_ids = np.unique(patientunitstayid)
+    # mean_proba = [np.mean(probas[np.where(patientunitstayid==id)]) for id in unique_ids]
 
-    # # create 2D array with patientunitstayid and mean proba values
-    result = np.column_stack((unique_ids, mean_proba))
-    np.savetxt('result.csv', result, delimiter=',')
+    # # # create 2D array with patientunitstayid and mean proba values
+    # result = np.column_stack((unique_ids, mean_proba))
+    # np.savetxt('result.csv', result, delimiter=',')
 
 
 if __name__ == "__main__":
--- model.py
+++ model.py
@@ -1,7 +1,8 @@
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.decomposition import PCA
-
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
 
 class Model():
     def __init__(self, n_neighbors: int):
@@ -10,29 +11,34 @@ class Model():
         # You can add arguements to the initialization as needed
 
         ########################################################################
-        self.neigh = KNeighborsClassifier(n_neighbors=n_neighbors)
-        self.cols = ['age', 'unitvisitnumber', 'offset', 'GCS Total', 'Heart Rate', 'O2 Saturation', 'Respiratory Rate', 'BP Mean']
+        self.neigh = RandomForestClassifier(n_estimators=500, max_depth=50, random_state=0)
+        # self.cols = ['age', 'unitvisitnumber', 'admissionweight', 'GCS Total', 'Heart Rate', 'O2 Saturation', 'Respiratory Rate', 'BP Mean']
 
     def fit(self, x_train, y_train, x_val=None, y_val=None):
         ############################ Your Code Here ############################
         # Fit your model to the training data here
 
         ########################################################################
-        x_train = x_train[self.cols]
-        x_val = x_val[self.cols]
-        self.pca = PCA(n_components=2)
-        self.pca.fit(x_train)
-        x_train = self.pca.transform(x_train)
-        x_val = self.pca.transform(x_val)
+        # x_train = x_train[self.cols]
+        # x_val = x_val[self.cols]
+        # self.pca = PCA(n_components=3)
+        # self.pca.fit(x_train)
+        # x_train = self.pca.transform(x_train)
+        # x_val = self.pca.transform(x_val)
         self.neigh.fit(x_train, y_train)
-        return self.neigh.score(x_val, y_val)
+        threshold = 0.3
+        predicted_proba = self.neigh.predict_proba(x_val)
+        predicted = (predicted_proba [:,1] >= threshold).astype('int')
+
+        accuracy = accuracy_score(y_val, predicted)
+        return accuracy
 
     def predict_proba(self, x):
         ############################ Your Code Here ############################
         # Predict the probability of in-hospital mortaility for each x
 
         ########################################################################
-        x =x[self.cols]
-        x = self.pca.transform(x)
-        probas = self.neigh.predict_proba(x)
-        return probas
+        # x =x[self.cols]
+        # x = self.pca.transform(x)
+        predicted = self.neigh.predict_proba(x)
+        return predicted
