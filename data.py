bbebb96524ee98665901d77488f471b31d6db603
BennettLai2 <bxl190020@tamu.edu>
2023-05-01 20:14:09 -0500

    Init. Im going to redo-data

--- data.py
+++ data.py
@@ -36,92 +36,85 @@ def preprocess_x(df):
     # Your code here
     # sort data by the following
     df = df.sort_values(by=['patientunitstayid', 'age', 'offset', 'nursingchartcelltypevalname'])
-    # forward filling the height and weight by mean of age, ethnicity, and gender
-    df['admissionheight'] = df.groupby(['age', 'ethnicity', 'gender'])['admissionheight'].transform(lambda x: x.fillna(x.mean()))
-    df['admissionweight'] = df.groupby(['age', 'ethnicity', 'gender'])['admissionweight'].transform(lambda x: x.fillna(x.mean()))
-    # drop missing lab results
-    mask = (~df['labname'].isnull()) & (df['labresult'].isnull())
-    df.drop(index=df.loc[mask].index, inplace=True)
-    # one-hot encode for celllabel
+    df['age'] = df['age'].replace(['> 89'], '89')
+
     df['cellattributevalue'] = df['cellattributevalue'].replace({'normal': 0, 
                                                                 '< 2 seconds': 1, 
                                                                 '> 2 seconds': 2, 
                                                                 'feet': 3, 
                                                                 'hands': 4})
-    # replace missing GCS for filling
-    df = df.replace('Unable to score due to medication', pd.NaT)
-    cols = ['admissionheight', 'admissionweight', 'age', 'ethnicity', 'gender', 'unitvisitnumber', 'nursingchartvalue']
-    df.loc[:,cols] = df.loc[:,cols].ffill()
-    # changing age to int dtype
-    df['age'] = df['age'].replace(['> 89'], '89')
-    df['age'] = df['age'].astype(int)
-    df['celllabel'] = df['celllabel'].fillna('Capillary Refill')
-    df['cellattributevalue'] = df['cellattributevalue'].fillna(-1)
+    df['ethnicity'] = df['ethnicity'].replace({'African American': 0, 
+                                               	'Asian': 1, 	
+                                                'Caucasian': 2, 	
+                                                'Hispanic': 3, 	
+                                                'Native American': 4, 	
+                                                'Other/Unknown': 5})
+    df['gender'] = df['gender'].replace({'Female': 0, 'Male': 1})
 
-    # df.to_csv('pre.csv')
-    
+    mask = (~df['labname'].isnull()) & (df['labresult'].isnull())
+    df.drop(index=df.loc[mask].index, inplace=True)
+
+    df = df.replace('Unable to score due to medication', pd.NaT)
+    mask = (~df['nursingchartcelltypevalname'].isnull()) & (df['nursingchartvalue'].isnull())
+    df.drop(index=df.loc[mask].index, inplace=True)
+
+    cols = ['admissionheight', 'admissionweight', 'age', 'ethnicity', 'gender', 'unitvisitnumber']
+    for col in cols: 
+        df[col] = df.groupby('patientunitstayid')[col].ffill()
+
+    df['ethnicity'] = df['ethnicity'].fillna(5)
+    df['gender'] = df['gender'].fillna(3)
+
+    for col in cols: 
+        mean = df[col].apply(pd.to_numeric, errors='coerce').mean()
+        df[col] = df.groupby('patientunitstayid')[col].fillna(mean)
+
     condensed_df1 = df.pivot_table(
-        index=['patientunitstayid', 'unitvisitnumber', 'offset', 'admissionheight', 'admissionweight', 'age', 'ethnicity', 'gender', 'hospitaldischargestatus'],
+        index=['patientunitstayid', 'offset', 'admissionheight', 'admissionweight', 'age', 'ethnicity', 'gender', 'unitvisitnumber'],
         columns=['nursingchartcelltypevalname'],
         values=['nursingchartvalue']
     ).reset_index()
     
     condensed_df2 = df.pivot_table(
-        index=['patientunitstayid', 'unitvisitnumber', 'offset', 'admissionheight', 'admissionweight', 'age', 'ethnicity', 'gender', 'hospitaldischargestatus'],
+        index=['patientunitstayid', 'offset', 'admissionheight', 'admissionweight', 'age', 'ethnicity', 'gender', 'unitvisitnumber'],
         columns=['labname'],
         values=['labresult']
     ).reset_index()
 
     condensed_df3 = df.pivot_table(
-        index=['patientunitstayid', 'unitvisitnumber', 'offset', 'admissionheight', 'admissionweight', 'age', 'ethnicity', 'gender', 'hospitaldischargestatus'],
+        index=['patientunitstayid', 'offset', 'admissionheight', 'admissionweight', 'age', 'ethnicity', 'gender', 'unitvisitnumber'],
         columns=['celllabel'],
         values=['cellattributevalue'], 
         aggfunc='min'
     ).reset_index()
     
-    condensed_df4 = pd.merge(condensed_df1, condensed_df2, on=['patientunitstayid', 'unitvisitnumber', 'offset', 'admissionheight', 'admissionweight', 'age', 'ethnicity', 'gender', 'hospitaldischargestatus'], how='outer')
-    condensed_df = pd.merge(condensed_df3, condensed_df4, on=['patientunitstayid', 'unitvisitnumber', 'offset', 'admissionheight', 'admissionweight', 'age', 'ethnicity', 'gender', 'hospitaldischargestatus'], how='outer')
+    condensed_df4 = pd.merge(condensed_df1, condensed_df2, on=['patientunitstayid', 'unitvisitnumber', 'offset', 'admissionheight', 'admissionweight', 'age', 'ethnicity', 'gender'], how='outer')
+    condensed_df = pd.merge(condensed_df3, condensed_df4, on=['patientunitstayid', 'unitvisitnumber', 'offset', 'admissionheight', 'admissionweight', 'age', 'ethnicity', 'gender'], how='outer')
     
-    condensed_df = condensed_df.sort_values(by=['patientunitstayid', 'offset'])
-    
-    condensed_df.columns = ['patientunitstayid', 'unitvisitnumber', 'offset', 'admissionheight', 'admissionweight', 'age', 'ethnicity', 'gender', 'hospitaldischargestatus', 'Capillary Refill', 'GCS Total', 'Heart Rate', 'Invasive BP Diastolic', 'Invasive BP Mean', 'Invasive BP Systolic', 'Non-Invasive BP Diastolic', 'Non-Invasive BP Mean', 'Non-Invasive BP Systolic', 'O2 Saturation', 'Respiratory Rate', 'glucose', 'pH']
-    
-    # condensed_df['Capillary Refill'] = condensed_df['Capillary Refill'].ffill()
-    # condensed_df['Capillary Refill'] = condensed_df['Capillary Refill'].fillna(0, inplace=True)
-    
-    condensed_df['Capillary Refill'] = condensed_df['Capillary Refill'].replace(-1, pd.NaT)
-    condensed_df['Capillary Refill'] = condensed_df.groupby('patientunitstayid')['Capillary Refill'].ffill().fillna(0)
-    condensed_df['Heart Rate'] = condensed_df.groupby('patientunitstayid')['Heart Rate'].ffill().bfill()
-    condensed_df['Heart Rate'] = condensed_df['Heart Rate'].fillna(condensed_df['Heart Rate'])
-    condensed_df['O2 Saturation'] = condensed_df.groupby('patientunitstayid')['O2 Saturation'].ffill().bfill()
-    condensed_df['O2 Saturation'] = condensed_df['O2 Saturation'].fillna(100)
-    condensed_df['Respiratory Rate'] = condensed_df.groupby('patientunitstayid')['Respiratory Rate'].ffill().bfill()
-    condensed_df['Respiratory Rate'] = condensed_df['Respiratory Rate'].fillna(18)
-    condensed_df['glucose'] = condensed_df.groupby('patientunitstayid')['glucose'].ffill().bfill()
-    condensed_df['glucose'] = condensed_df['glucose'].fillna(86.6)
-    condensed_df['pH'] = condensed_df.groupby('patientunitstayid')['pH'].ffill().bfill()
-    condensed_df['pH'] = condensed_df['pH'].fillna(7.4)
-    
-    condensed_df['GCS Total'] = condensed_df.groupby('patientunitstayid')['GCS Total'].ffill().fillna(15)
-
-    combined_df = condensed_df[['Invasive BP Diastolic', 'Invasive BP Mean', 'Invasive BP Systolic', 'Non-Invasive BP Diastolic', 'Non-Invasive BP Mean', 'Non-Invasive BP Systolic']].copy()
+    df = condensed_df.sort_values(by=['patientunitstayid', 'offset'])
+    df.columns = ["patientunitstayid","offset","admissionheight","admissionweight","age","ethnicity","gender","unitvisitnumber","Capillary Refill","GCS Total","Heart Rate","Invasive BP Diastolic","Invasive BP Mean","Invasive BP Systolic","Non-Invasive BP Diastolic","Non-Invasive BP Mean","Non-Invasive BP Systolic","O2 Saturation","Respiratory Rate","glucose","pH"]
+    combined_df = df[['Invasive BP Diastolic', 'Invasive BP Mean', 'Invasive BP Systolic', 'Non-Invasive BP Diastolic', 'Non-Invasive BP Mean', 'Non-Invasive BP Systolic']].copy()
     combined_df['BP Diastolic'] = combined_df['Invasive BP Diastolic'].combine_first(combined_df['Non-Invasive BP Diastolic'])
     combined_df['BP Mean'] = combined_df['Invasive BP Mean'].combine_first(combined_df['Non-Invasive BP Mean'])
     combined_df['BP Systolic'] = combined_df['Invasive BP Systolic'].combine_first(combined_df['Non-Invasive BP Systolic'])
     combined_df = combined_df[['BP Diastolic', 'BP Mean', 'BP Systolic']]
 
-    condensed_df.drop(['Invasive BP Diastolic', 'Invasive BP Mean', 'Invasive BP Systolic', 'Non-Invasive BP Diastolic',
+    df.drop(['Invasive BP Diastolic', 'Invasive BP Mean', 'Invasive BP Systolic', 'Non-Invasive BP Diastolic',
                        'Non-Invasive BP Mean', 'Non-Invasive BP Systolic'], axis=1, inplace=True)
-    condensed_df = pd.concat([condensed_df, combined_df], axis=1)
-    condensed_df['BP Diastolic'] = condensed_df.groupby('patientunitstayid')['BP Diastolic'].ffill().bfill()
-    condensed_df['BP Diastolic'] = condensed_df['BP Diastolic'].fillna(condensed_df['BP Diastolic'])    
-    condensed_df['BP Systolic'] = condensed_df.groupby('patientunitstayid')['BP Systolic'].ffill().bfill()
-    condensed_df['BP Systolic'] = condensed_df['BP Systolic'].fillna(condensed_df['BP Systolic'])   
-    condensed_df['BP Mean'] = condensed_df['BP Mean'].fillna(condensed_df['BP Diastolic'].shift(1) + (condensed_df['BP Systolic'].shift(-1) - condensed_df['BP Diastolic'].shift(1))/3)
-    condensed_df=condensed_df.ffill().bfill()
-    categorical = condensed_df.select_dtypes(exclude=['int64', 'float64'])
-    numerical = condensed_df.select_dtypes(include=['int64', 'float64'])
-    cate_dummies = pd.get_dummies(categorical)
-    condensed_df = pd.concat([cate_dummies, numerical], axis=1)
+    df = pd.concat([df, combined_df], axis=1)
+    df['BP Diastolic'] = df.groupby('patientunitstayid')['BP Diastolic'].ffill().bfill()
+    df['BP Diastolic'] = df['BP Diastolic'].fillna(df['BP Diastolic'].mean())    
+    df['BP Systolic'] = df.groupby('patientunitstayid')['BP Systolic'].ffill().bfill()
+    df['BP Systolic'] = df['BP Systolic'].fillna(df['BP Systolic'].mean())   
+    df['BP Mean'] = df['BP Mean'].fillna(df['BP Diastolic'].shift(1) + (df['BP Systolic'].shift(-1) - df['BP Diastolic'].shift(1))/3)
+    
+    cols = ["Capillary Refill","GCS Total","Heart Rate","O2 Saturation","Respiratory Rate","glucose","pH"]
+    vals = [0, 15, 80, 100, 18, 86.5, 7.4]
+    for col in cols: 
+        df[col] = df.groupby('patientunitstayid')[col].ffill().bfill()
+    for i in range(len(cols)): 
+        df[cols[i]] = df.groupby('patientunitstayid')[cols[i]].fillna(vals[i])
 
-    return condensed_df
+    df = df.ffill().bfill()
+    df.to_csv('data.csv', index=False)
+    return df
--- main.py
+++ main.py
@@ -40,20 +40,18 @@ class DeviceDataLoader():
         return len(self.dl)
 
 def main():
-    # args = parse()
-    # device = get_default_device()
-    # print(device)
 
-    merged_df = load_data("processed_train_x.csv")
+    x = load_data("train_x.csv")
+    x = preprocess_x(x)
     # x = load_data("train_x.csv")
     # y = load_data("train_y.csv")
     # merged_df = pd.merge(x, y[['patientunitstayid', 'hospitaldischargestatus']], on='patientunitstayid')
     # merged_df = preprocess_x(merged_df)
     # merged_df = merged_df.reindex(columns =['patientunitstayid', 'hospitaldischargestatus', 'ethnicity_African American', 'ethnicity_Asian', 'ethnicity_Caucasian', 'ethnicity_Hispanic', 'ethnicity_Native American', 'ethnicity_Other/Unknown', 'gender_Female', 'gender_Male', 'unitvisitnumber', 'offset', 'admissionheight', 'admissionweight', 'age', 'Capillary Refill', 'GCS Total', 'Heart Rate', 'O2 Saturation', 'Respiratory Rate', 'glucose', 'pH', 'BP Diastolic', 'BP Mean', 'BP Systolic'])
     # merged_df.to_csv('processed_train_x.csv', index=False)
-    y = merged_df[['hospitaldischargestatus']].values.ravel()
-    # np.savetxt('y.csv', y, delimiter=',')
-    x = merged_df.drop('hospitaldischargestatus', axis=1)
+    # y = merged_df[['hospitaldischargestatus']].values.ravel()
+    # # np.savetxt('y.csv', y, delimiter=',')
+    # x = merged_df.drop('hospitaldischargestatus', axis=1)
 
 
 if __name__ == "__main__":
