9c327c61052d7fe296284b19300614bda06de5cf
BWSullivan <brandon71399@gmail.com>
2023-05-02 19:54:44 -0500

    LSTM Update

--- main.py
+++ main.py
@@ -3,18 +3,16 @@
 # data = create_data_for_project(".")
 
 import itertools
- 
+import numpy as np
+
 import torch
-import torch.nn as nn
-import torch.optim as optim
 import pandas as pd
 from sklearn.metrics import roc_auc_score
-
+from sklearn.model_selection import StratifiedKFold
 
 from data import load_data, preprocess_x, split_data
 from parser_1 import parse
-from model import Model, MortalityDataset, train_model
-from torch.utils.data import DataLoader, Dataset
+from model import Model
 
 
 def get_default_device():
@@ -23,10 +21,12 @@ def get_default_device():
     else:
         return torch.device('cpu')
 
+
 def to_device(data, device):
     if isinstance(data, (list, tuple)):
         return [to_device(x, device) for x in data]
     return data.to(device, non_blocking=True)
+
 
 class DeviceDataLoader():
     def __init__(self, dl, device):
@@ -40,37 +40,41 @@ class DeviceDataLoader():
     def __len__(self):
         return len(self.dl)
 
+
 def main():
-    args = parse()
+    # x = load_data("train_x.csv")
+    # x = preprocess_x(x)
+    # x.to_csv('processed_train_x.csv')
+    x = load_data("processed_train_x.csv")
+    y = load_data('train_y.csv')
+
+    y = pd.DataFrame({'hospitaldischargestatus': x['patientunitstayid'].map(
+        y.set_index('patientunitstayid')['hospitaldischargestatus'])})
     device = get_default_device()
-    print(device)
 
-    x = load_data("train_x.csv")
-    y = load_data("train_y.csv")
-    merged_df = pd.merge(x, y[['patientunitstayid', 'hospitaldischargestatus']], on='patientunitstayid')
-    processed_x_train = preprocess_x(merged_df)
-    # print(processed_x_train.dtypes)
-    # processed_x_test = preprocess_x(y)
+    train_x, test_x, train_y, test_y = split_data(x, y)
 
-    # train_x, train_y, test_x, test_y = split_data(x, y)
+    # -------------------------
+    skf = StratifiedKFold(n_splits=5, shuffle=False)
 
-    train_dataset = MortalityDataset(processed_x_train, y)
-    train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True, num_workers=2)
-
-    input_size = processed_x_train.shape[1]
-    hidden_size = 64
-    num_layers = 3
+    input_size = train_x.shape[1] - 1  # Minus 1 to exclude 'patientunitstayid'
+    hidden_size = 1024
+    num_layers = 2
     output_size = 1
-    model = Model(input_size, hidden_size, num_layers, output_size).to(device)
 
-    criterion = nn.BCEWithLogitsLoss()
-    optimizer = optim.Adam(model.parameters(), lr=0.0001)
+    for train_index, test_index in skf.split(x, y):
+        X_train, X_test = x.iloc[train_index], x.iloc[test_index]
+        Y_train, Y_test = y.iloc[train_index], y.iloc[test_index]
 
-    num_epochs = 10
-    for epoch in range(num_epochs):
-        train_loss = train_model(model, train_loader, criterion, optimizer, device)
-        print(f"Epoch {epoch + 1}, Loss: {train_loss:.4f}")
+        cv_model = Model(input_size, hidden_size, num_layers, output_size, device)  # you can add arguments as needed
+        acc = cv_model.fit(X_train, Y_train, X_test, Y_test)
+        print(acc)
+
+    model = Model(input_size, hidden_size, num_layers, output_size, device)  # you can add arguments as needed
+    acc = model.fit(train_x, train_y, test_x, test_y)
+    print(acc)
 
 
 if __name__ == "__main__":
     main()
+
--- model.py
+++ model.py
@@ -1,76 +1,76 @@
-import torch.nn as nn
 import torch
-import torch.nn.functional as F
-from torch.utils.data import DataLoader, Dataset
+import torch.nn as nn
+import torch.optim as optim
+import numpy as np
+import pandas as pd
 
-class Model(nn.Module):
-    def __init__(self, input_size, hidden_size, num_layers, output_size):
-        ############################ Your Code Here ############################
-        # Initialize your model in this space
-        # You can add arguements to the initialization as needed
-        super(Model, self).__init__()
+class LSTMModel(nn.Module):
+    def __init__(self, input_size, hidden_size, num_layers, output_size, device):
+        super(LSTMModel, self).__init__()
         self.hidden_size = hidden_size
         self.num_layers = num_layers
-
+        self.device = device
         self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True)
         self.fc = nn.Linear(hidden_size, output_size)
 
-        ########################################################################
-
     def forward(self, x):
-        ############################ Your Code Here ############################
-        # Fit your model to the training data here
-        x = x.unsqueeze(1)
-        h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(x.device)
-        c0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(x.device)
+        x = x.to(self.device)
+        h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(self.device)
+        c0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(self.device)
 
         out, _ = self.lstm(x, (h0, c0))
         out = self.fc(out[:, -1, :])
         return out
 
 
-        ########################################################################
+class Model():
+    def __init__(self, input_size, hidden_size, num_layers, output_size, device, lr=0.05, num_epochs=240):
+        self.device = device
+        self.model = LSTMModel(input_size, hidden_size, num_layers, output_size, device).to(device)
+        self.criterion = nn.BCEWithLogitsLoss()
+        self.optimizer = optim.Adam(self.model.parameters(), lr=lr)
+        self.num_epochs = num_epochs
+
+    def fit(self, x_train, y_train, x_val=None, y_val=None):
+        x_train_np = x_train.drop('patientunitstayid', axis=1).values.astype(np.float64)
+        y_train_np = y_train.values.ravel().astype(np.float64)
+        x_val_np = x_val.drop('patientunitstayid', axis=1).values.astype(np.float64)
+        y_val_np = y_val.values.ravel().astype(np.float64)
+
+        x_train, y_train, x_val, y_val = torch.tensor(x_train_np).float().to(self.device), torch.tensor(
+            y_train_np).float().to(self.device), torch.tensor(x_val_np).float().to(self.device), torch.tensor(
+            y_val_np).float().to(self.device)
+
+        for epoch in range(self.num_epochs):
+            self.model.train()
+            self.optimizer.zero_grad()
+            outputs = self.model(x_train.unsqueeze(1))
+            loss = self.criterion(outputs.squeeze(), y_train)
+            loss.backward()
+            self.optimizer.step()
+
+            self.model.eval()
+            with torch.no_grad():
+                val_outputs = self.model(x_val.unsqueeze(1))
+                val_loss = self.criterion(val_outputs.squeeze(), y_val)
+
+            if (epoch+1) % 10 == 0:
+                print(f'Epoch [{epoch+1}/{self.num_epochs}], Loss: {loss.item():.4f}, Val Loss: {val_loss.item():.4f}')
+
+        return val_loss.item()
 
     def predict_proba(self, x):
-        ############################ Your Code Here ############################
-        # Predict the probability of in-hospital mortaility for each x
-        with torch.no_grad():
-            logits = self.forward(x)
-            probabilities = F.softmax(logits, dim=1)
-            return probabilities
+        # Check for non-numeric columns
+        non_numeric_columns = x.select_dtypes(include=['object']).columns
+        if not non_numeric_columns.empty:
+            print(f"Warning: Non-numeric columns found: {non_numeric_columns}. Please preprocess the data accordingly.")
+            return None
 
-        ########################################################################
+        # Check for missing values
+        if x.isna().any():
+            print("Warning: Missing values found. Please preprocess the data accordingly.")
+            return None
 
-class MortalityDataset(Dataset):
-    def __init__(self, data, labels):
-        self.data = data
-        self.labels = labels
-
-    def __len__(self):
-        return len(self.data)
-
-    def __getitem__(self, index):
-        # Assuming data and labels are pandas DataFrames
-        x = torch.tensor(self.data.iloc[index].values, dtype=torch.float32)
-        y = torch.tensor(
-            self.labels.loc[self.labels['patientunitstayid'] == self.data.iloc[index]['patientunitstayid']][
-                'hospitaldischargestatus'].values[0], dtype=torch.float32)
-
-        return x, y
-
-def train_model(model, train_loader, criterion, optimizer, device):
-    model.train()
-    running_loss = 0.0
-
-    for inputs, labels in train_loader:
-        inputs, labels = inputs.to(device), labels.to(device)
-        optimizer.zero_grad()
-
-        outputs = model(inputs)
-        loss = criterion(outputs.squeeze(), labels)
-        loss.backward()
-        optimizer.step()
-
-        running_loss += loss.item()
-
-    return running_loss / len(train_loader)
+        x = torch.tensor(x.values, dtype=torch.float32).to(self.device)
+        probas = self.model(x).cpu().numpy()
+        return probas
